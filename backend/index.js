521e9999a5ab410152129659bb7a8d939c35988a
shubhamxthakur <stfushubham@gmail.com>
2024-08-15 14:31:20 +0530

    added express router, cors -5th step done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const app = express();
+const cors = require("cors");
 const { user } = require("./db");
 const rootRouter = require("./routes/index");
 
-const app = express();
 const PORT = 3000;
 
+app.use(cors);
+app.use(express.json());
 app.use("api/v1/", rootRouter);
 
 app.listen(PORT, (err) => {
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const rootRouter = express.Router();
 const userRouter = require("./user");
+const accountRouter = require("./account");
 
 rootRouter.use("/user", userRouter);
+rootRouter.use("/account", accountRouter);
 
 module.exports = rootRouter;
